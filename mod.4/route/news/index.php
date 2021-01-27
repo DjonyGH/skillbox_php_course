dce3905533feb6b462c39c2fc02bec191b82deac
Евгений Калимуллин <counsil1986@gmail.com>
2021-01-27 16:32:11 +0700

    mod.4 added pages about-us, contacts, news,  katalog, info

---
+++ mod.4/route/news/index.php
@@ -0,0 +1,10 @@
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT'].'/templates/header.php');
+?>
+
+<h1>Новости</h1>
+<p>Здесь будет контент страницы "Новости".</p>
+
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT'].'/templates/footer.php');
+?>
